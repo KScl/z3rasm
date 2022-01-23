KatDevsGames/z3randomizer

Stats: Track chest turns

stats.asm
@@ -1,7 +1,7 @@
 ;================================================================================
 ; Stat Tracking
 ;================================================================================
-; $7EF420 - $7EF466 - Stat Tracking
+; $7EF420 - $7EF468 - Stat Tracking
 ;--------------------------------------------------------------------------------
 ; $7EF420 - bonk counter
 ;--------------------------------------------------------------------------------
@@ -136,6 +136,8 @@
 ; $7EF464w[2] - mirror timestamp (low)
 ;--------------------------------------------------------------------------------
 ; $7EF466w[2] - mirror timestamp (high)
+;--------------------------------------------------------------------------------
+; $7EF468 - chest turn counter
 ;--------------------------------------------------------------------------------
 
 ;--------------------------------------------------------------------------------
@@ -320,6 +322,15 @@ IncrementFairyRevivalCounter:
 	PHA
 		LDA !LOCK_STATS : BNE +
 			LDA !FAIRY_COUNTER : INC : STA !FAIRY_COUNTER
+		+
+	PLA
+RTL
+;--------------------------------------------------------------------------------
+!CHESTTURN_COUNTER = "$7EF468"
+IncrementChestTurnCounter:
+	PHA
+		LDA !LOCK_STATS : BNE +
+			LDA !CHESTTURN_COUNTER : INC : STA !CHESTTURN_COUNTER
 		+
 	PLA
 RTL

KScl/z3rasm

stats.asm
@@ -1,7 +1,7 @@
 ;================================================================================
 ; Stat Tracking
 ;================================================================================
-; $7EF420 - $7EF466 - Stat Tracking
+; $7EF420 - $7EF468 - Stat Tracking
 ;--------------------------------------------------------------------------------
 ; $7EF420 - bonk counter
 ;--------------------------------------------------------------------------------
@@ -116,7 +116,9 @@
 ;--------------------------------------------------------------------------------
 ; $7EF46Bw[2] - Dungeon prizes collected by dungeon location.
 ;--------------------------------------------------------------------------------
-; $7EF46D - 7EF49F - Free space
+; $7EF46D - chest turn counter
+;--------------------------------------------------------------------------------
+; $7EF46E - 7EF49F - Free space
 ;--------------------------------------------------------------------------------
 ; $7EF4A0 - 7EF4A7 - Service Request
 ;--------------------------------------------------------------------------------
@@ -322,6 +324,15 @@ IncrementFairyRevivalCounter:
 	PHA
 		LDA !LOCK_STATS : BNE +
 			LDA !FAIRY_COUNTER : INC : STA !FAIRY_COUNTER
+		+
+	PLA
+RTL
+;--------------------------------------------------------------------------------
+!CHESTTURN_COUNTER = "$7EF46D"
+IncrementChestTurnCounter:
+	PHA
+		LDA !LOCK_STATS : BNE +
+			LDA !CHESTTURN_COUNTER : INC : STA !CHESTTURN_COUNTER
 		+
 	PLA
 RTL